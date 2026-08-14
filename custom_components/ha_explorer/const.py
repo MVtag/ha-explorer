ee19d62a0bde16949218e10bc78ef3367516c124
MVtag <marc@vojer-tagesen.dk>
2026-08-14 14:24:54 +0200

    Add integration constants

---
+++ custom_components/ha_explorer/const.py
@@ -0,0 +1,12 @@
+"""Constants for Home Assistant Explorer."""
+
+DOMAIN = "ha_explorer"
+PLATFORMS = ["sensor"]
+
+CONF_DEVICE_ID = "device_id"
+CONF_MODEL = "model"
+
+DEFAULT_NAME = "Home Assistant Explorer"
+MAX_TARGET_SLOTS = 10
+LIVE_TRACK_REFRESH_SECONDS = 50
+RECONNECT_DELAY_SECONDS = 5
